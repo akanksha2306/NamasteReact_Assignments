2dd8af73f110cae2df36b4205df9cec084dccae9
Akanksha <kanjoliaakanksha@gmail.com>
2024-01-31 16:04:03 +0530

    some cool stuff

--- App.js
+++ App.js
@@ -1,10 +1,25 @@
 import React from "react";
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 
-const heading = React.createElement("h1",{id:"heading"},
-"Hello world from react!");
+const Title = () =>(
+<h1>Namaste react using jsx3</h1>
+)
 
+
+//React functional component
+//component composition
+const HeadingComponent = () =>{
+    return(
+        <div id = "container">
+           {Title()}
+           <Title></Title>
+           <Title />
+            <h1>Namaste react functional component</h1>
+            
+        </div>
+
+    )
+}
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(heading);
-
+root.render(<HeadingComponent />)
